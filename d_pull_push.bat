f357922016ee367cdd9d591f635f6d866a5fce7b
liuhuihe <lhhriver@163.com>
2022-10-11 21:15:12 +0800

    update

---
+++ d_pull_push.bat
@@ -0,0 +1,41 @@
+@echo off
+
+echo ==============  Update Ehe  ==============
+D:
+cd D:\Gitee\Ehe
+
+echo:
+git add .
+
+echo:
+git commit -m "update"
+
+echo:
+echo ==============  Pull  Ehe  =============
+git pull origin master
+
+echo:
+git push origin master
+
+
+
+echo:
+echo ==============  Update DDU  ==============
+D:
+cd D:\Gitee\DDU
+
+echo:
+git add .
+
+echo:
+git commit -m "update"
+
+echo:
+echo ==============  Pull DDU  ==============
+git pull origin master
+
+echo:
+git push origin master
+
+echo:
+pause
