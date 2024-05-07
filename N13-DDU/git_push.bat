b071f50214e2601e5d0d40998cec7a8eef850af8
liuhuihe <lhhriver@163.com>
2024-05-07 09:54:26 +0800

    updated by Ehe at 2024-05-07 09:54:26.656

--- N13-DDU/git_push.bat
+++ N13-DDU/git_push.bat
@@ -5,20 +5,21 @@ echo:
 git add .
 
 echo:
-git commit -m "update"
+for /f "tokens=*" %%a in ('WMIC OS GET LocalDateTime ^| find "."') do set datetime=%%a
+set datetime=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2% %datetime:~8,2%:%datetime:~10,2%:%datetime:~12,6%
+set GIT_COMMITTER_DATE=%datetime%
+git commit -m "updated by Ehe at %GIT_COMMITTER_DATE%"
 
 echo:
 git push origin master
-
 
 echo:
 git add .
 
 echo:
-git commit -m "update"
+git commit -m "updated by Ehe at %GIT_COMMITTER_DATE%"
 
 echo:
 git push origin master
-
 echo:
 pause
